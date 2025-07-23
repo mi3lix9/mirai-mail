2b890ea1fd90613705bb12a5ffd5eb988d4abe0a
Mighty Ali <Mi3liX9@Outlook.sa>
2025-07-23 21:37:18 +0300

    add db and redis

---
+++ apps/server/src/db/auth.ts
@@ -0,0 +1,52 @@
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable("users", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: integer("email_verified", { mode: "boolean" })
+    .$defaultFn(() => !1)
+    .notNull(),
+  image: text("image"),
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .$defaultFn(() => new Date())
+    .notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp" })
+    .$defaultFn(() => new Date())
+    .notNull(),
+});
+
+export const accounts = sqliteTable("accounts", {
+  id: text("id").primaryKey(),
+  accountId: text("account_id").notNull(),
+  providerId: text("provider_id").notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  accessToken: text("access_token"),
+  refreshToken: text("refresh_token"),
+  idToken: text("id_token"),
+  accessTokenExpiresAt: integer("access_token_expires_at", {
+    mode: "timestamp",
+  }),
+  refreshTokenExpiresAt: integer("refresh_token_expires_at", {
+    mode: "timestamp",
+  }),
+  scope: text("scope"),
+  password: text("password"),
+  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).notNull(),
+});
+
+export const verifications = sqliteTable("verifications", {
+  id: text("id").primaryKey(),
+  identifier: text("identifier").notNull(),
+  value: text("value").notNull(),
+  expiresAt: integer("expires_at", { mode: "timestamp" }).notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" }).$defaultFn(
+    () => new Date(),
+  ),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).$defaultFn(
+    () => new Date(),
+  ),
+});
--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,20 +1,44 @@
 import { expo } from "@better-auth/expo";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db";
-import * as schema from "../db/schema/auth";
+import { redis } from "bun";
+import * as schema from "../db/auth";
+import { db } from "./db";
 
 export const auth = betterAuth({
-	database: drizzleAdapter(db, {
-		provider: "sqlite",
-
-		schema,
-	}),
-	trustedOrigins: [process.env.CORS_ORIGIN || "", "my-better-t-app://"],
-	emailAndPassword: {
-		enabled: true,
-	},
-	secret: process.env.BETTER_AUTH_SECRET,
-	baseURL: process.env.BETTER_AUTH_URL,
-	plugins: [expo()],
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+    usePlural: true,
+    schema,
+  }),
+  secondaryStorage: {
+    get: async (key) => redis.get(key),
+    set: async (key, value, ttl) => {
+      await redis.set(key, value);
+      if (ttl) {
+        await redis.expire(key, ttl);
+      }
+    },
+    delete: async (key) => {
+      await redis.del(key);
+    },
+  },
+  trustedOrigins: [process.env.CORS_ORIGIN || "", "my-better-t-app://"],
+  emailAndPassword: {
+    enabled: true,
+  },
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  },
+  microsoft: {
+    clientId: process.env.MICROSOFT_CLIENT_ID as string,
+    clientSecret: process.env.MICROSOFT_CLIENT_SECRET as string,
+    // Optional
+    // tenantId: "common",
+    // prompt: "select_account", // Forces account selection
+  },
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
+  plugins: [expo()],
 });
