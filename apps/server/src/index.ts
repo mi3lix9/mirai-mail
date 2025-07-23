e5af5c5739472800b1c3e6310a1b01cac44ed3a7
Mighty Ali <Mi3liX9@Outlook.sa>
2025-07-23 20:53:00 +0300

    add pino in elysia

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,33 +5,35 @@ import { Elysia } from "elysia";
 import { auth } from "./lib/auth";
 import { createContext } from "./lib/context";
 import { appRouter } from "./routers";
+import { log } from "./utils/log";
 
 const handler = new RPCHandler(appRouter);
 
 const _app = new Elysia()
-	.use(
-		cors({
-			origin: process.env.CORS_ORIGIN || "",
-			methods: ["GET", "POST", "OPTIONS"],
-			allowedHeaders: ["Content-Type", "Authorization"],
-			credentials: true,
-		})
-	)
-	.all("/api/auth/*", (context) => {
-		const { request } = context;
-		if (["POST", "GET"].includes(request.method)) {
-			return auth.handler(request);
-		}
-		context.status(405);
-	})
-	.all("/rpc*", async (context) => {
-		const { response } = await handler.handle(context.request, {
-			prefix: "/rpc",
-			context: await createContext({ context }),
-		});
-		return response ?? new Response("Not Found", { status: 404 });
-	})
-	.get("/", () => "OK")
-	.listen(3000, () => {
-		// Server is running
-	});
+  .use(log.into())
+  .use(
+    cors({
+      origin: process.env.CORS_ORIGIN || "",
+      methods: ["GET", "POST", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  )
+  .all("/api/auth/*", (context) => {
+    const { request } = context;
+    if (["POST", "GET"].includes(request.method)) {
+      return auth.handler(request);
+    }
+    context.status(405);
+  })
+  .all("/rpc*", async (context) => {
+    const { response } = await handler.handle(context.request, {
+      prefix: "/rpc",
+      context: await createContext({ context }),
+    });
+    return response ?? new Response("Not Found", { status: 404 });
+  })
+  .get("/", () => "OK")
+  .listen(3000, () => {
+    // Server is running
+  });
